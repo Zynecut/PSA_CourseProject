b77d5370d09eb4b6cb10749ddb4b0039fa79d92b
AleksanderVS <106428709+Zynecut@users.noreply.github.com>
2023-09-29 11:00:16 +0200

    Lagt til noen classes i functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,21 @@
 import math
 
-class Functions:
+class NewtonRaphsonLF:
     def funcPi(v_i, v_j, y_ij, theta_ij, delta_i, delta_j):
         P = abs(v_i*v_j*y_ij)*math.sin(theta_ij + delta_j - delta_i)
         return P
+
+
+class DecouplingLF:
+    def funcP():
+        return None
+    
+
+class FastDecouplingLF:
+    def funcPyay():
+        return None
+    
+
+class DCLF:
+    def funcPDC():
+        return None
