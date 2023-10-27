New and imporved jacobian, should be correct now

NRLF.py
@@ -19,13 +19,13 @@ def NewtonRaphson():
 
     bus_overview = setupBusType(bus_data)
     P_spec, Q_spec = findKnowns(bus_data, Sbase)
-    v_guess, diraq_guess = findUnknowns(bus_overview, bus_data)
+    v_guess, dirac_guess = findUnknowns(bus_overview, bus_data)
 
     deltaP = calcP(BusList, P_spec, YBus, Sbase)
     deltaQ = calcQ(BusList, Q_spec, YBus, Sbase)
     knowns = np.concatenate((deltaP, deltaQ), axis= 0)
 
-    jacobian_matrix = buildJacobian(BusList, P_spec, Q_spec, v_guess, diraq_guess, YBus)
+    jacobian_matrix = buildJacobian(BusList, P_spec, Q_spec, v_guess, dirac_guess, YBus)
     unknowns = calcDeltaUnknowns(jacobian_matrix, knowns)
     # print(unknowns)
 
@@ -34,8 +34,8 @@ def NewtonRaphson():
     df_ybus = pd.DataFrame(YBus)
     print(df_ybus, "\n\n", df_jac)
 
-    updateVoltageAndAngleList(unknowns, diraq_guess, v_guess)
-    updateBusList(BusList, diraq_guess, v_guess)
+    updateVoltageAndAngleList(unknowns, dirac_guess, v_guess)
+    updateBusList(BusList, dirac_guess, v_guess)
 
 
 if __name__ == '__main__':